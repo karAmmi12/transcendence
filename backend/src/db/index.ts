30dfaaf38e7d1047c2eebcf3990965deb5fa51bf
Alessandro Pintus <apintus@f2r8s12.clusters.42paris.fr>
2025-07-25 19:08:18 +0200

    mise ene place database.db

---
+++ backend/src/db/index.ts
@@ -0,0 +1,15 @@
+import Database from "better-sqlite3";
+
+const db = new Database("database.db", {verbose: console.log}); //affiche dans la console pour debug
+
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        createdAt TEXT DEFAULT CURRENT_TIMESTAMP
+    );
+`);
+
+export default db;
