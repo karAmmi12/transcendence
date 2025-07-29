764663fea116c33b154fff1c179b2da6dcbeb7d2
Thomas Steve Passin <tpassin@f2r9s10.clusters.42paris.fr>
2025-07-29 18:30:52 +0200

    modif et instructions avec mamadou

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
+// installer node path, et virer tout ce qui est dirname
+// mettre juste un alias @ si non vous allez trop vous perdre
+// ne pas mettre tout le build context par defaut vite build dans dist
+// pas de vite en prod (interdit) donc pas de vite preview
+// proxy vers localhost:(port-du-backend) pour le dev et pour la prod osef
+
+
+
 export default defineConfig({
   root: '.',
   publicDir: 'public',
@@ -9,11 +17,12 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://backend:8000',
+        // striper /api/ 
+        target: 'http://backend:8000', //localhost
         changeOrigin: true,
         secure: false
       },
-      '/socket.io': {
+      '/socket.io': { //mettre ws ou socket pas de point
         target: 'http://backend:8000',
         changeOrigin: true,
         ws: true
