f088ddb5ead1f41a6e820007aa7b0e74c4de5226
Alessandro Pintus <apintus@f2r8s14.clusters.42paris.fr>
2025-08-05 18:07:05 +0200

    ajout schema fastify pour register et login

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import db from "../db/index.js";
 import bcrypt from "bcrypt"
+import { loginSchema, registerSchema } from "../schemas/auth_schema.js";
+import { RegisterData, LoginData } from "../types/auth.js";
+import { loginUser, registerUser } from "../services/authServices.js";
+import { log } from "console";
 
 export default async function authRoutes(app: FastifyInstance) {
     
@@ -10,29 +14,56 @@ export default async function authRoutes(app: FastifyInstance) {
         return { message: 'Auth route works!' };
     });
 
-    // Route de login (exemple)
-    app.post('/login', async (req: FastifyRequest, reply: FastifyReply) => {
-        return { message: 'Login endpoint' };
-    });
+    // register ANCIENNE VERSION
+    // app.post("/register", async (req: FastifyRequest, reply: FastifyReply) => {
+    //     const { email, password } = req.body as { email: string; password: string};
 
-    // register
-    app.post("/register", async (req: FastifyRequest, reply: FastifyReply) => {
-        const { email, password } = req.body as { email: string; password: string};
+    //     if (!email || !password) {
+    //         return (reply.status(400).send({error: "Email and password required"}));
+    //     }
 
-        if (!email || !password) {
-            return (reply.status(400).send({error: "Email and password required"}));
-        }
+    //     const hashed = await bcrypt.hash(password, 10);
 
-        const hashed = await bcrypt.hash(password, 10);
+    //     try {
+    //   const stmt = db.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+    //   stmt.run(email, hashed);
 
-        try {
-      const stmt = db.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
-      stmt.run(email, hashed);
+    //   reply.send({ message: "✅ User created" });
+    // } catch (err) {
+    //   reply.status(400).send({ error: "Email already exists" });
+    // }
+  // });
 
-      reply.send({ message: "✅ User created" });
-    } catch (err) {
-      reply.status(400).send({ error: "Email already exists" });
-    }
+
+  //route register afin d'ajouter un nouveau user
+  app.post('/register', {schema: registerSchema}, async (req: FastifyRequest, reply: FastifyReply) => {
+
+      const userData = req.body as RegisterData;
+
+      const result = await registerUser(userData);
+      if (!result.success)
+          return (reply.status(400).send({error: result.error}));
+      
+      reply.status(201).send({
+        message: "✅ User created successfully",
+        user: result.user
+      });
+  });
+
+  //route login permet au user de ce connecter
+  app.post('/login', {schema: loginSchema}, async (req: FastifyRequest, reply: FastifyReply) => {
+    
+      const loginData = req.body as LoginData;
+
+      const result = await loginUser(loginData);
+      if (!result.success) 
+        return (reply.status(401).send({error: result.error}));
+
+      reply.send({
+          message: "✅ Login successful",
+          user: result.user
+      })
+
   });
 
   // Get all users
---
+++ backend/src/types/auth.d.ts
@@ -0,0 +1,62 @@
+// validation
+// export interface ValidationResult {
+//     isValid: boolean;
+//     errors: string[];
+// }
+
+// export interface SignupBody {
+//     username?: string;
+//     email: string;
+//     password: string;
+// }
+
+export interface RegisterData {
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginData {
+  identifier: string; //siuu email ou username pour l'instant
+  password: string;
+}
+
+export interface AuthResult {
+  success: boolean;
+  user?: {
+    id: number;
+    username: string;
+    email: string;
+    avatar?: string;
+    isOnline?: boolean;
+    twoFactorEnabled?: boolean;
+    createdAt?: string;
+    stats?: UserStats;
+  };
+  token?: string;
+  error?: string;
+}
+
+export interface UserStats {
+  wins: number;
+  losses: number;
+  totalGames: number;
+  winRate: number;
+  rank: number;
+  highestScore: number;
+  currentStreak: number;
+  longestStreak: number;
+}
+
+// interface pour recupe info de la db
+interface UserFromDB {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  avatar?: string;
+  isOnline: boolean;
+  twoFactorEnabled: boolean;
+  createdAt: string;
+  lastLogin?: string;
+}
