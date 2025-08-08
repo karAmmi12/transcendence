a751b2a8370f88e718a88b4ad6bd36c5a31a1034
Alessandro Pintus <apintus@f2r7s11.clusters.42paris.fr>
2025-08-08 14:11:43 +0200

    ajout des cookies pour les tokens JWT

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -10,8 +10,17 @@ db.exec(`
         password TEXT NOT NULL,
         createdAt TEXT DEFAULT CURRENT_TIMESTAMP,
         avatar_url TEXT,
-        is_online INTEGER NOT NULL DEFAULT 0
+        is_online INTEGER NOT NULL DEFAULT 0,
+        lastLogin TEXT
     );
+    CREATE TABLE IF NOT EXISTS sessions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            refresh_token TEXT NOT NULL UNIQUE,
+            expires_at DATETIME NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+        );
 `);
 
 
