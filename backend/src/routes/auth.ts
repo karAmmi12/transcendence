3ae3324a412625161d4973cd93e7b8e578be71b5
Alessandro Pintus <alesshardy22@gmail.com>
2025-08-11 15:55:03 +0200

    normalement plus de conflit identifier

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -51,7 +51,6 @@ export default async function authRoutes(app: FastifyInstance) {
     console.log("JE SUIS DANS LE BACKENd /LOGIN");
 
     const loginData = req.body as LoginData;
-    console.log("identifier: ", loginData.identifier);
     
     const result = await loginUser(loginData);
     if (!result.success) 
