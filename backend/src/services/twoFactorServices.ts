0958da08a967d3cbc063b56f825ad92c8943c630
Thomas Steve Passin <tpassin@f2r7s11.clusters.42paris.fr>
2025-08-22 19:12:08 +0200

    add twoFactor debut

--- backend/src/services/twoFactorServices.ts
+++ backend/src/services/twoFactorServices.ts
@@ -1,6 +1,38 @@
-import { UserFromDB } from "../types/auth";
+import { UserTwoFactor, } from "../types/auth";
+import nodemailer from 'nodemailer';
+import db from "../db/index.js";
+import 'dotenv/config';
 
 export class TwoFactorServices
 {
-    
+    static async getUserById(userId: number): Promise<UserTwoFactor | null>
+    {
+        try {
+
+            const stmt = db.prepare("SELECT id, email, two_factor_enabled, google_id FROM users where id = ?");
+            const user = stmt.get(userId) as any | undefined;
+            if (!user)
+                return (null);
+            console.log(`Getting 2FA user info for userId: ${userId}`);            
+            const userData = {
+                id: user.id,
+                email: user.email,
+                twoFactorEnabled: user.two_factor_enabled,
+                googleId: user.google_id,
+            };
+            return (userData as UserTwoFactor);
+
+        }catch (error) {
+            throw new Error('Get userinfo 2FA failed request');
+        }
+    }
+
+    static async sendEmail(email: string, code: string): Promise<boolean>
+    {
+        const transporter = nodemailer.createTransport({
+            auth: {
+                user:
+            }
+        })
+    }
 }
