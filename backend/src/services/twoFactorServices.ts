384ca5fde6d3c999fef0c31dfdd36185c8fa97e9
Thomas Steve Passin <passin.thomas96@gmail.com>
2025-08-26 19:02:18 +0200

    2FA ok il faut juste la clean

--- backend/src/services/twoFactorServices.ts
+++ backend/src/services/twoFactorServices.ts
@@ -1,12 +1,12 @@
 
-import { UserTwoFactor } from "../types/auth";
+import { UserTwoFactor, TwoFactorToken } from "../types/auth";
 import nodemailer from 'nodemailer';
 import db from "../db/index.js";
 import crypto from "crypto";
+import bcrypt from 'bcrypt'; 
 import 'dotenv/config';
 
 export class TwoFactorServices {
-    private static storeTwoFactor = new Map<number, { code: string, expiresAt: Date }>();
 
     static async getUserById(userId: number): Promise<UserTwoFactor | null>
     {
@@ -37,7 +37,7 @@ export class TwoFactorServices {
     {
         try {
             const transporter = nodemailer.createTransport({
-                host: "smtp.office365.com",
+                host: "smtp.gmail.com",
                 port: 587,
                 secure: false,
                 auth: {
@@ -46,15 +46,40 @@ export class TwoFactorServices {
                 },
             });
             await transporter.sendMail({
-                from: `"Ft_transcendence <${process.env.MAIL_2FA}>`,
+                from: `"Ft_transcendence`,
                 to: email,
                 subject: "Code de verification",
                 text: `Bonjour,\n\nVotre code de validation est disponible ci-dessous et sera valable pendant 10 minutes :\n\nCODE : ${code}\n\nMerci de ne pas partager ce code avec qui que ce soit.\n\nCordialement,\nL’équipe Ft_transcendence`
             });
             return (true);
-        } catch {
-            console.error('error: Failed to send mail for 2FA');
+        } catch (error) {
+            console.error('error: Failed to send mail for 2FA', error);
             return (false);
+        }
+    }
+
+    static async storeTwoFactor(userId: number, code: string): Promise<TwoFactorToken | null>
+    {
+        try {
+            
+            const codeHash = await bcrypt.hash(code, 10);
+            const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
+            const stmt = db.prepare(`
+                INSERT OR REPLACE INTO two_factor_tokens 
+                (user_id, token, expires_at)
+                VALUES (?, ?, ?)`);
+            const res = stmt.run(userId, codeHash, expiresAt.toString());
+            const returnUser = {
+                id: userId,
+                token: codeHash,
+                expiresAt: expiresAt
+            } as TwoFactorToken;
+            console.log("GET userinfo Two Factor: ", returnUser);
+            return (returnUser);
+
+        }catch (error){
+            console.error("Failed query DB 2FA");
+            return (null);
         }
     }
 
@@ -65,57 +90,92 @@ export class TwoFactorServices {
             return { success: false, message: 'User not found' };
         if (user.googleId)
             return { success: false, message: '2FA not available for Google users' };
-
+        
         const code = this.generateCode();
-        const tokenUser = TwoFactorServices.getProfile(code, user.id);
-        this.storeTwoFactor.set(user.id, {
-            code,
-            expiresAt: new Date(Date.now() + 10 * 60 * 1000)
-        });
+        const tokenUser = await this.storeTwoFactor(userId, code);
+
+        if (!tokenUser)
+            return ({ success: false, message: "error get TokenUser in DB" });
         const emailValid = await this.sendEmail(user.email, code);
         if (!emailValid)
             return { success: false, message: 'Failed to send email for 2FA' };
         return { success: true, message: '2FA code sent by email' };
     }
 
+    static async getUserInfo(userId: number): Promise<TwoFactorToken | null>
+    {
+        try {
+            const stmt = db.prepare("SELECT * FROM two_factor_tokens WHERE user_id = ? ");
+            const tokenData = stmt.get(userId) as any;
+            console.log("OUAIS", tokenData);
+            if (!tokenData) 
+                return null;
+            return {
+                id: tokenData.user_id,
+                token: tokenData.token,
+                expiresAt: new Date(tokenData.expires_at)
+            } as TwoFactorToken;
+
+        }catch(error) {
+            console.error('Error getting 2FA token:', error);
+            return null;
+        }
+    }
+
     static async verifyCode(userId: number, code: string): Promise<{ success: boolean, message: string }> 
     {
-        const stored = this.storeTwoFactor.get(userId);
+        const stored = await this.getUserInfo(userId);
         if (!stored)
             return { success: false, message: 'No code found for this user' };
         if (stored.expiresAt < new Date()) {
-            this.storeTwoFactor.delete(userId);
+            console.log("Code expired for 2FA");
             return { success: false, message: 'Code expired' };
         }
-        if (stored.code !== code)
+        
+        // Utiliser bcrypt.compare pour comparer le code
+        const isValidCode = await bcrypt.compare(code, stored.token);
+        if (!isValidCode)
             return { success: false, message: 'Incorrect code' };
 
         // Enable 2FA in DB if not already enabled
         const user = await this.getUserById(userId);
-        if (user && !user.twoFactorEnabled) {
+        if (user) {
             const stmt = db.prepare("UPDATE users SET two_factor_enabled = 1 WHERE id = ?");
-            stmt.run(userId);
+            stmt.run(user.id);
+            
+            // Supprimer le token utilisé
+            const deleteStmt = db.prepare("DELETE FROM two_factor_tokens WHERE user_id = ?");
+            deleteStmt.run(userId);
+            
             console.log(`2FA allowed for user: ${user.email}`);
+            return { success: true, message: '2FA validated successfully' };
+        } else {
+            return {success: false, message: "User not found"};
         }
-        this.storeTwoFactor.delete(userId);
-        return { success: true, message: '2FA validated successfully' };
     }
 
-    static async disableTwoFactor(userId: number, code: string): Promise<{ success: boolean, message: string }> {
-        const stored = this.storeTwoFactor.get(userId);
-        if (!stored)
+    static async disableTwoFactor(userId: number, code: string): Promise<{ success: boolean, message: string }> 
+    {
+        const stored = await this.getUserInfo(userId);
+        if (!stored?.token)
             return { success: false, message: 'No code found for this user' };
         if (stored.expiresAt < new Date()) {
-            this.storeTwoFactor.delete(userId);
             return { success: false, message: 'Code expired' };
         }
-        if (stored.code !== code)
+        
+        // Utiliser bcrypt.compare pour comparer le code
+        const isValidCode = await bcrypt.compare(code, stored.token);
+        if (!isValidCode)
             return { success: false, message: 'Incorrect code' };
 
         // Disable 2FA in DB
         const stmt = db.prepare("UPDATE users SET two_factor_enabled = 0 WHERE id = ?");
         stmt.run(userId);
-        this.storeTwoFactor.delete(userId);
+        
+        // Supprimer le token utilisé
+        const deleteStmt = db.prepare("DELETE FROM two_factor_tokens WHERE user_id = ?");
+        deleteStmt.run(userId);
+        
         console.log(`2FA disabled`);
         return { success: true, message: '2FA disabled successfully' };
     }
