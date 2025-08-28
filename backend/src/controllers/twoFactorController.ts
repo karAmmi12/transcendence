2ba22ed9f817bd321ae25930387f55df3fad6f8e
Thomas Steve Passin <tpassin@f2r7s13.clusters.42paris.fr>
2025-08-28 18:38:03 +0200

    2FA debug front et back pour la fonction enable

--- backend/src/controllers/twoFactorController.ts
+++ backend/src/controllers/twoFactorController.ts
@@ -2,21 +2,18 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { TwoFactorServices } from "../services/twoFactorServices.js";
 
 export class TwoFactorController {
-    static async enableTwoFactor(req: FastifyRequest, reply: FastifyReply) 
+    static async sendTwoFactor(req: FastifyRequest, reply: FastifyReply) 
     {
         
         try {
-
-            // const { userId } = req.body as { userId: number };
-            const user = req.user!;
-            const userId = user.userId; 
+            const { userId } = req.user as { userId: number};
 
             const result = await TwoFactorServices.sendCode(userId);
             return reply.status(200).send({ success: true, message: result.message });
         } catch (error) {
             if (error instanceof Error)
                 console.error(error.message);
-            return reply.status(500).send({ error: error || "Internal error sending 2FA code" });
+            return reply.status(500).send({ error: "Internal error sending 2FA code" });
         }
     }
 
@@ -25,10 +22,10 @@ export class TwoFactorController {
 
         try {
             const { userId } = req.user as {userId: number};
-            // const userId = user.userId;
-            const { code }  = req.body as { code: string };
+            const { code, disabled }  = req.body as { code: string, disabled: boolean };
 
-            const result = await TwoFactorServices.verifyCode(userId, code, false);
+            console.log("Code reçu:", code, "Disabled:", disabled);
+            const result = await TwoFactorServices.verifyCode(userId, code, disabled);
             if (!result.success)
                 return reply.status(400).send({ error: result.message });
             return reply.status(200).send({ success: true, message: result.message });
@@ -36,22 +33,6 @@ export class TwoFactorController {
             if (error instanceof Error)
                 console.error(error.message);
             return reply.status(500).send({ error: "Internal error verifying 2FA code" });
-        }
-    }
-
-    static async disableTwoFactor(req: FastifyRequest, reply: FastifyReply) 
-    {
-        
-        try {
-            const user = req.user!;
-            const userId = user.userId;
-            const { code }  = req.body as { code: string };
-            const result = await TwoFactorServices.verifyCode(userId, code, true);
-            return reply.status(200).send({ success: true, message: result.message });
-        } catch (error) {
-            if (error instanceof Error)
-                console.error(error.message);
-            return reply.status(500).send({ error: error || "Internal error disabling 2FA" });
         }
     }
 }
