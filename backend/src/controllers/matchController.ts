a95bea8cabe1ceb0dece0af220dbf82805f50d42
Alessandro Pintus <alesshardy22@gmail.com>
2025-09-01 16:34:11 +0200

    ajout fonction register match backend

--- backend/src/controllers/matchController.ts
+++ backend/src/controllers/matchController.ts
@@ -4,45 +4,37 @@ import { MatchService } from "../services/matchServices";
 export class MatchController
 {
     /**
-     * Route pour creer un match simple local
+     * route pour en registrer un match local terminer
      */
-    static async createLocalMatch(req: FastifyRequest, reply: FastifyReply)
+    static async registerLocalMatch(req: FastifyRequest, reply: FastifyReply)
     {
         try {
             const user = req.user; //pas de ! car peux etre null 
             
-            const {player1, player2}=req.body as {player1: string; player2: string};
-            if (!player1 || !player2)
+            const {player1, player2, score1, score2, duration} = req.body as {player1: string; player2: string, score1: number, score2: number, duration: number};
+            if (!player1 || !player2 || !score1 || !score2)
             {
                 return (reply.status(400).send({
                     success: false,
-                    error: "both player required"
+                    error: "both player and score required"
                 }))
             }
             
-            const match = await MatchService.createLocalMatch(player1, player2, user?.userId);
+            const match = await MatchService.createLocalMatch(player1, player2,score1, score2, duration, user?.userId);
 
             return (reply.status(200).send({
                 success: true,
-                message: "local match create",
+                message: "local match register",
                 match
             }));
 
         } catch (error) {
-            console.error("Create local match error:", error);
+            console.error("Register local match error:", error);
             return (reply.status(500).send({
                 success: false,
-                error: "Failed to create match"
+                error: "Failed to register match"
             }));
         }
-    }
-
-    /**
-     * ROute pour finir un match simple local 
-     */
-    static async finishLocalMatch(req: FastifyRequest, reply: FastifyReply)
-    {
-        //siuu a faire
     }
 
 }
