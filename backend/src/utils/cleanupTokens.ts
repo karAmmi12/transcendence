0345a1d710c13cd2c6bb52fa5b72028673679130
Thomas Steve Passin <passin.thomas96@gmail.com>
2025-09-01 14:55:39 +0200

    add cleanupToken

--- backend/src/utils/cleanupTokens.ts
+++ backend/src/utils/cleanupTokens.ts
@@ -9,10 +9,10 @@ interface TokenRow {
 // Toutes les 10 minutes, supprime les tokens expirés dans le 2FA
 cron.schedule('*/10 * * * *', () => {
   const tokens = db.prepare('SELECT user_id, expires_at FROM two_factor_tokens').all() as TokenRow[];
-  const now = new Date(Date.now() + 0 * 60 * 1000).toISOString();
+  const now = new Date(Date.now()).toISOString();
 
   tokens.forEach(token => {
-    if (new Date(token.expires_at).toISOString() < now) {
+    if (token.expires_at < now) {
       db.prepare('DELETE FROM two_factor_tokens WHERE user_id = ?').run(token.user_id);
       console.log(`Token expiré supprimé pour user_id: ${token.user_id}`);
     }
