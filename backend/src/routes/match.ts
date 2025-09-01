f20b30f8f5a0552d6630796dcc7313fe6ef4b54e
Alessandro Pintus <apintus@f2r7s13.clusters.42paris.fr>
2025-09-01 16:34:11 +0200

    ajout fonction register match backend

--- backend/src/routes/match.ts
+++ backend/src/routes/match.ts
@@ -4,8 +4,9 @@ import { MatchController } from "../controllers/matchController";
 export default async function matchRoutes(app: FastifyInstance)
 {
     // routes proterger par le middleware
-    app.post('/local/create', MatchController.createLocalMatch);
-    app.put('/local/:matchId/finish', MatchController.finishLocalMatch);
+    app.post('/local/register', MatchController.registerLocalMatch);
+    // app.post('/local/create', MatchController.createLocalMatch);
+    // app.put('/local/:matchId/finish', MatchController.finishLocalMatch);
     // app.get('/:id', MatchController.getLocalMatch);
 
 }
--- backend/src/types/match.d.ts
+++ backend/src/types/match.d.ts
@@ -1,10 +1,15 @@
+// export interface MatchResponse {
+//     id: number;
+//     mode: 'local' | 'online' | 'tournament'
+//     player1: Player;
+//     player2: Player;
+//     status: 'in_progress' | 'finished'
+//     createdAt: string;
+// }
+
 export interface MatchResponse {
-    id: number;
-    mode: 'local' | 'online' | 'tournament'
-    player1: Player;
-    player2: Player;
-    status: 'in_progress' | 'finished'
-    createdAt: string;
+    success: boolean;
+    message?: string;
 }
 
 export interface Player {
