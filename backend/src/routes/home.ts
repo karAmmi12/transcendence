9eefc26ca6d0a9f53295ac529429fd6838e0e764
Karim AMMI <karim.ammi@hotmail.com>
2025-09-26 17:19:40 +0200

    Revert "Merge pull request #1 from karAmmi12/apintus"
    This reverts commit 4f2a00eaca718d2f3e589424f5b40d1ee07385a6, reversing
    changes made to 5ed45c1dc4c2d1159572d4dd1a7380fd72334c30.

--- backend/src/routes/home.ts
+++
@@ -1,8 +0,0 @@
-import {FastifyInstance} from "fastify";
-import { HomeController } from "../controllers/homeController.js";
-
-export default async function homeRoutes(app: FastifyInstance)
-{
-    // routes proterger par le middleware
-    app.get('/stats', HomeController.getGeneralStats);
-}
--- backend/src/routes/twoFactorRoute.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify"
-import { TwoFactorController } from "../controllers/twoFactorController.js"
-
-export default async function twoFaRoutes (app: FastifyInstance) {
-    app.post('/enabled', TwoFactorController.sendTwoFactor);
-    app.post('/verify', TwoFactorController.validateTwoFactor);
-    app.post('/disabled', TwoFactorController.sendTwoFactor);
-
-}
